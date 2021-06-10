99b78e3cfa6e508b430cde8f7cd7659ab542c08f
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-06-11 00:01:43 +0700

    create navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css'
+
+function index() {
+  return (
+    <nav className={styles.nav}>
+      <h1 className={styles.navTitle}>Todo App</h1>
+    </nav>
+  )
+}
+
+export default index

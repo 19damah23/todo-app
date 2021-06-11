9a1e217d2bb5a958771b3905aef46c28c758df21
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-06-11 09:25:56 +0700

    create container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar/index.js";
+import Container from "./components/Container/index.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Container>
+
+      </Container>
     </div>
   );
 }
